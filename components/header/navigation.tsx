2b7cf5f4ae0fc76924286c9cc8746b198b81566c
anggara.setiawan <aanggaadc@gmail.com>
2024-06-10 17:06:02 +0700

    add box shadow

--- components/header/navigation.tsx
+++ components/header/navigation.tsx
@@ -6,7 +6,7 @@ export default function Navigation() {
       <ul className="flex items-center">
         <li>
           <Link to="form" spy={true} smooth={true} duration={500}>
-            <button className="font-montserrat text-sm md:text-base p-2 rounded-md text-white bg-[linear-gradient(to_left,#074864,#074e6a,#08536f,#085975,#095f7a)]">
+            <button className="font-montserrat shadow-md text-sm md:text-base p-2 rounded-lg text-white bg-[linear-gradient(to_left,#074864,#074e6a,#08536f,#085975,#095f7a)]">
               Dukung Bang Amsakar
             </button>
           </Link>
